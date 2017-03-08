cb15512514f768c0052cb8c41560ac8762dcfdaa
Sam Greenwood <sam@samgreenwood.me>
2017-03-08 16:14:09 +1030

    validate profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,7 +20,23 @@ class ProfileController extends Controller
     public function store()
     {
         $this->validate(request(), [
-
+            'member.firstname' => 'required',
+            'member.surname' => 'required',
+            'member.birthday' => 'required|date',
+            'member.wia_member' => 'required',
+            'member.affiliated_club' => 'required',
+            'member.forward_email' => 'required',
+            'member.occupation' => 'required',
+            'member.postal_address_1' => 'required',
+            'member.postal_address_state' => 'required',
+            'member.postal_address_suburb' => 'required',
+            'member.postal_address_postcode' => 'required',
+            'member.postal_address_country' => 'required',
+            'member.billing_address_1' => 'required',
+            'member.billing_address_state' => 'required',
+            'member.billing_address_suburb' => 'required',
+            'member.billing_address_postcode' => 'required',
+            'member.billing_address_country' => 'required',
         ]);
 
         auth()->user()->update(request('member'));
