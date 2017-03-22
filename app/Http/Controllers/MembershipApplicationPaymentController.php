f739cbbbcdaf9ec8042e6d0e52b4b3243adc7106
Sam Greenwood <sam@samgreenwood.me>
2017-03-22 22:13:40 +1030

    add braintree processing and config

--- app/Http/Controllers/MembershipApplicationPaymentController.php
+++ app/Http/Controllers/MembershipApplicationPaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Membership;
 use App\Payment;
 use App\User;
+use Braintree_Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -27,25 +28,40 @@ class MembershipApplicationPaymentController extends Controller
      */
     public function store(Request $request)
     {
-        $user = User::findByApprovalToken($request->input('approval_token'));
-
-        $payment = Payment::create([
-            'user_id' => $user->id,
-            'type' => 'Credit Card',
-            'amount' => $request->amount,
-            'reference' => $request->reference,
-            'date' => Carbon::now(),
+        $result = Braintree_Transaction::sale([
+            'amount' => config('membership.rate'),
+            'paymentMethodNonce' => $request->input('payment_method_nonce'),
+            'options' => [
+                'submitForSettlement' => True
+            ]
         ]);
 
-        Membership::create([
-            'user_id' => $user->id,
-            'payment_id' => $payment->id,
-            'start' => Carbon::now(),
-            'end' => Carbon::now()->addYear()
-        ]);
+        if($result->success)
+        {
+            $user = User::findByApprovalToken($request->input('approval_token'));
 
-        auth()->login($user);
+            $payment = Payment::create([
+                'user_id' => $user->id,
+                'type' => 'Credit Card',
+                'amount' => $result->transaction->amount,
+                'reference' => $result->transaction->id,
+                'date' => Carbon::now(),
+            ]);
 
-        return redirect()->route('dashboard')->with('message', 'Payment successful, welcome!');
+            Membership::create([
+                'user_id' => $user->id,
+                'payment_id' => $payment->id,
+                'start' => Carbon::now(),
+                'end' => Carbon::now()->addYear()
+            ]);
+
+            auth()->login($user);
+
+            return redirect()->route('dashboard')->with('message', 'Payment successful, welcome!');
+        }
+
+        return redirect()->back()->with('error', $result->transaction->status);
+
+
     }
 }
--- config/braintree.php
+++ config/braintree.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-
+    'environment' => env('BRAINTREE_ENVIRONMENT'),
+    'merchant_id' => env('BRAINTREE_MERCHANT_ID'),
+    'public_key' => env('BRAINTREE_PUBLIC_KEY'),
+    'private_key' => env('BRAINTREE_PRIVATE_KEY')
 ];
