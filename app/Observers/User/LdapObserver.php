9090c296fb989b29cfc99645d1502125bd341627
Sam Greenwood <sam@samgreenwood.me>
2017-03-29 09:23:54 +1030

    add crypt before password in user ldap sync

--- app/Observers/User/LdapObserver.php
+++ app/Observers/User/LdapObserver.php
@@ -51,7 +51,7 @@ class LdapObserver
                 $info['objectclass'][3] = 'posixAccount';
                 $info['objectclass'][4] = 'qmailUser';
                 $info['uid'] = $user->username;
-                $info['userpassword'] = $user->password;
+                $info['userpassword'] = '{crypt}' . $user->password;
                 $info['givenname'] = $user->firstname;
                 $info['sn'] = $user->lastname;
                 $info['cn'] = $user->firstname.' '.$user->lastname;
