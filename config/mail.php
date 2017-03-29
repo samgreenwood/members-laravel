d956bd07b287a732438ee5053add17c0ef55f9b9
Sam Greenwood <sam@samgreenwood.me>
2017-03-29 22:24:30 +1030

    fix pw redirect and mail from

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'committee@air-stream.org'),
+        'name' => env('MAIL_FROM_NAME', 'Air-Stream Committee'),
     ],
 
     /*
